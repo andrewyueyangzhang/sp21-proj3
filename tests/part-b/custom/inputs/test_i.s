b74722a05db9020c36dd5a0ac0c561ff96c6ac8e
Andrew Zhang <andrewzhang2001@gmail.com>
2021-04-03 14:54:15 -0700

    completed proj3

--- tests/part-b/custom/inputs/test_i.s
+++ tests/part-b/custom/inputs/test_i.s
@@ -22,26 +22,48 @@ ori x11 x1 -4
 andi x11 x1 4
 andi x12 x1 -4
 
-addi x13 x0 111
-addi x14 x1 -50
-addi x15 x0 111
-addi x16 x1 -50
-addi x17 x0 111
-addi x18 x1 -50
-addi x19 x0 111
-addi x20 x1 -50
-addi x21 x0 111
-addi x22 x1 -50
-addi x23 x0 111
-addi x24 x1 -50
-addi x25 x0 111
-addi x26 x1 -50
-addi x27 x1 -50
-addi x28 x0 111
-addi x29 x1 -50
-addi x30 x0 111
-addi x31 x1 -50
+addi x0 x0 0
+addi x1 x1 111
+addi x2 x2 -50
+addi x3 x3 111
+addi x4 x4 -50
+addi x5 x5 111
+addi x6 x6 -50
+addi x7 x7 111
+addi x8 x8 -50
+addi x9 x9 111
+addi x10 x10 -50
+addi x11 x11 111
+addi x12 x12 -50
+addi x13 x13 111
+addi x14 x14 -50
+addi x15 x15 111
+addi x16 x16 -50
+addi x17 x17 111
+addi x18 x18 -50
+addi x19 x19 111
+addi x20 x20 -50
+addi x21 x21 111
+addi x22 x22 -50
+addi x23 x23 111
+addi x24 x24 -50
+addi x25 x25 111
+addi x26 x26 -50
+addi x27 x27 -50
+addi x28 x28 111
+addi x29 x29 -50
+addi x30 x30 111
+addi x31 x31 -50
 
 
 auipc sp 0x100
 auipc sp 0x100
+
+
+
+addi s0 x0 -2048
+lui s0 1048575
+
+
+lui a0, 0xdeada
+addi a0, a0, -0x111
--- tests/part-b/custom/inputs/test_j.s
+++ tests/part-b/custom/inputs/test_j.s
@@ -1,10 +1,34 @@
 jal ra first_jump
-jal ra 0x3FFF0
-lui s1 555
-jal x0 second_jump
-
 
 first_jump:
-    jalr x0 ra 0
+    jal ra second_jump
 second_jump: 
-    addi s1 x0 -555
+    jal ra third_jump
+
+third_jump:
+    addi s1 x0 5
+    addi s0 x0 0
+    jalr s1 ra fourth_jump
+
+fourth_jump: 
+    addi s0 s0 1
+    beq s0 s1 exit
+    jalr x0 ra fourth_jump
+
+
+fifth:
+    addi s1 x0 5
+    addi s0 x0 0
+    jal ra sixth
+
+sixth: 
+    addi s0 s0 1
+    beq s0 s1 exit
+    jal ra sixth
+
+exit:
+    jal ra 16384
+
+
+
+
